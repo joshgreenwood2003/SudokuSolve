20e853f3ac3c65ff67f1ff10a84e0386b7759c60
Josh Greenwood <joshgreenwood2003@gmail.com>
2022-09-12 14:02:54 +0100

    fixes

--- main.py
+++ main.py
@@ -1,38 +1,74 @@
 import copy
-
+filled = []
 options = []
 for i in range(9):
+    filled.append([])
     options.append([])
     for j in range(9):
+        filled[i].append(False)
         options[i].append([1,2,3,4,5,6,7,8,9])
 
+
+
 def removefrom(assignment,i,j,possible):
-    print("ORIGINAL")
-    print(assignment)
-    print("Now isolating " + str(possible))
+   # print("GOT" + str(possible) +" "+ str(i) +" "+ str(j))
+   # print(assignment)
+
     new = copy.deepcopy(assignment)
     new[i][j] = [possible]
     for rows in range(9):
+        if possible in new[rows][j] and rows!= i:
+            new[rows][j].remove(possible)
+    for cols in range(9):
+        if possible in new[i][cols] and cols != j:
+            new[i][cols].remove(possible)
+
+
+    for rows in range(9):
         for cols in range(9):
-            if (rows == i or cols == j or (rows//3 == i//3 and cols//3 == i //3)) and not(rows == i and cols == j):
-                if possible in new[i][j]:
-                    new[i][j].remove(possible)
-    print(new)
+            if ((rows//3 == i//3 and cols//3 == j //3)) and not(rows == i and cols == j):
+                if possible in new[rows][cols]:
+                    new[rows][cols].remove(possible)
+    #print("NOW")
+    #print(new)
     return new
 
-def solve(assignment):
-    for i,row in enumerate(assignment):
-        for j,cell in enumerate(row):
+def solve(assignment,completed):
+    cancont = False
+    for row in assignment:
+        for cell in row:
             if len(cell) == 0:
                 return False
-            elif len(cell) > 1:
+    for row in completed:
+        for item in row:
+            if item == False:
+                cancont = True
+    if cancont == False:
+        return assignment
+    for i,row in enumerate(assignment):
+        for j,cell in enumerate(row):
+            if len(cell) > 1:
                 for potentialval in cell:
                     newassignment = removefrom(assignment,i,j,potentialval)
-                    ans = solve(newassignment)
+                    completed[i][j] = True
+                    ans = solve(newassignment,completed)
                     if ans != False:
                         return ans
-    return assignment
-                            
+    return False
 
-sol = solve(options)
+
+
+
+
+
+
+
+def prettyprint(assignment):
+    for row in assignment:
+        for col in row:
+            print(str(col) + "   |   ", end = "")
+        print("\n")    
+                                  
+
+sol = solve(options,filled)
 print(sol)
